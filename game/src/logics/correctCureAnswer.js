b2f51192d273143cabbb37d09c867910b96a4a50
Kalita Mikhail <mkalita1991@gmail.com>
2019-01-08 23:21:44 +0300

    refactor: remove some files

--- game/src/logics/correctCureAnswer.js
+++
@@ -1,9 +0,0 @@
-export default function correctCureAnswer() {
-  const startHealth = 100;
-  const showMainHeroHP = document.querySelector('.mainHero-HP');
-  const cure = Math.floor(Math.random() * startHealth);
-  let currMainHeroHP = localStorage.getItem('mainHero');
-  currMainHeroHP = +(currMainHeroHP) + cure;
-  localStorage.setItem('mainHero', currMainHeroHP);
-  showMainHeroHP.innerHTML = `${currMainHeroHP} HP`;
-}
